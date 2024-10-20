f4f422e217ba2c565d6e1f4e4ef1df50c6d32387
jin <rainakim8318@gmail.com>
2024-10-20 21:24:50 +0900

    수정

--- vuestagram/src/components/FilterBox.vue
+++ vuestagram/src/components/FilterBox.vue
@@ -1,6 +1,6 @@
 <template>
 
-        <div :class="`filter-item ${filterData}`"   @click="$emit('filterClass', filterData)" :style="`background-image: url(${uploadimg})`">{{ filterData }}</div> 
+        <div :class="`filter-item + ${filterData}`"   @click="$emit('filterClass', filterData)" :style="`background-image: url(${uploadimg})`">{{ filterData }}</div> 
     
 </template>
 
